5773455c7068a3228305ee1b4abd2316445b6bb5
Prabeshika <prabeshika14@gmail.com>
2023-11-24 12:40:48 +0100

    Mandatory Iterations completed

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,68 @@
-// Soldier
-class Soldier {}
+// Iteration 1: Soldier
+class Soldier {
+  constructor(health, strength) {
+    this.health = health;
+    this.strength = strength;
+  }
 
-// Viking
-class Viking {}
+  attack() {
+    return this.strength;
+  }
 
-// Saxon
-class Saxon {}
+  receiveDamage(damage) {
+    // Remove the received damage from the health property
+
+    this.health -= damage;
+  }
+}
+
+// Iteration 2: Viking
+class Viking extends Soldier {
+  constructor(name, health, strength) {
+    super(health, strength);
+    this.name = name;
+  }
+
+  receiveDamage(damage) {
+    super.receiveDamage(damage);
+
+    if (this.health > 0) {
+      return `${this.name} has received ${damage} points of damage`;
+    } else {
+      return `${this.name} has died in act of combat`;
+    }
+  }
+
+  // Override the atatck method to return the strength property of the viking
+  attack() {
+    return this.strength;
+  }
+
+  battleCry() {
+    return `Odin Owns You All!`;
+  }
+}
+
+// Iteration 3: Saxon
+class Saxon extends Soldier {
+  constructor(health, strength) {
+    super(health, strength);
+  }
+
+  attack() {
+    return this.strength;
+  }
+
+  receiveDamage(damage) {
+    super.receiveDamage(damage);
+
+    if (this.health > 0) {
+      return `A Saxon has received ${damage} points of damage`;
+    } else {
+      return `A Saxon has died in combat`;
+    }
+  }
+}
 
 // War
 class War {}
